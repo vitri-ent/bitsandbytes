80a70aa9d18606db67ee1e9751e3f56b8869d329
jllllll <3887729+jllllll@users.noreply.github.com>
2023-05-25 17:25:47 -0500

    Add scipy as package dependency

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
     url="https://github.com/TimDettmers/bitsandbytes",
     packages=find_packages(),
     package_data={"": libs},
+    install_requires=['scipy'],
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
     classifiers=[
